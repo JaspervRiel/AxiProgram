2027177f0b11d995208adbf82c440712b697a953
Yordi Verschure <459372@student.fontys.nl>
2022-09-28 14:07:35 +0200

    NavBar with borders

--- Frontend/axi/src/Components/Navbar.js
+++ Frontend/axi/src/Components/Navbar.js
@@ -4,16 +4,23 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function ColorSchemesExample() {
   return (
-    
     <>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="white" variant="white" className="square border-bottom border-5">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img
+                alt=""
+                src="https://www.partnersfontysict.nl/site/assets/files/1074/axi-logo.-logo_medium.jpg"
+                width="77"
+                height="34"
+                className="d-inline-block align-top"
+              />{' '}
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="About">About</Nav.Link>
+            <Nav.Link href="pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
