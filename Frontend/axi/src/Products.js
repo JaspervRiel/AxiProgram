d2e2fd07f26aa24425351e8818831a54baff60d8
Riel,Jasper J. van <jasper.vanriel@student.fontys.nl>
2022-09-28 14:08:25 +0200

    added table to components

---
+++ Frontend/axi/src/Products.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Table from './Components/Table'
+function Products(){
+return(
+    <div className="">
+    <Table/>
+    </div>
+)
+}
+
+export default Products;
