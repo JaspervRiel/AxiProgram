92e08f7640621f00541a4a0ab28e99b08a5f8392
MiesvanGogh <93597938+MiesvanGogh@users.noreply.github.com>
2022-09-28 14:45:45 +0200

    3 lagen model + GetAll en Create. GetAll heeft ook unit tests

---
+++ Backend/DalMSSQL/Database.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DalMSSQL
+{
+    public class Database
+    {
+        private string connectiestring;
+        SqlConnection cnn;
+        SqlCommand command;
+        SqlDataReader reader;
+
+        private readonly string connect;
+
+        /// <summary>
+        /// Met deze constructor set ik de connectionstring
+        /// </summary>
+        /// <param name="cs"> De connectionstring </param>
+        public Database(string cs)
+        {
+            connect = cs;
+        }
+
+        public Database()
+        {
+            connectiestring = "Server=mssqlstud.fhict.local;Database=dbi485146_AXI;User Id=dbi485146_AXI;Password=Welkom01;";
+        }
+        public void databaseconnection(string sql)
+        {
+            cnn = new SqlConnection(connectiestring);
+            cnn.Close();
+            cnn.Open();
+            command = new SqlCommand(sql, cnn);
+        }
+    }
+}
---
+++ Backend/InterfaceLib/IProductContainer.cs
@@ -0,0 +1,12 @@
+﻿namespace InterfaceLib
+{
+    public interface IProductContainer
+    {
+        public List<ProductDTO> Getproducts();
+        public void Create(ProductDTO product);
+        public void Delete(int id);
+        public void Update(ProductDTO product);
+        public ProductDTO GetProductById(int id);
+
+    }
+}
