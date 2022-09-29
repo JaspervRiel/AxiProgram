d1a15b92a152c999a13baf62d5db1eb2051a6d3b
Yordi Verschure <459372@student.fontys.nl>
2022-09-29 09:57:56 +0200

    Navbar.css

--- Frontend/axi/src/Components/Navbar.js
+++ Frontend/axi/src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Navbar.css';
 
 function ColorSchemesExample() {
   return (
