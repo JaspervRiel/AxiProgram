01ac74551c72dc22fe60c4ef9ac039d1425d4476
Bas Bruin <487790@student.fontys.nl>
2022-09-29 10:34:38 +0200

    Scaled table

--- Frontend/axi/src/Components/Table.jsx
+++ Frontend/axi/src/Components/Table.jsx
@@ -23,7 +23,7 @@ const rows = [
 export default function BasicTable() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 550 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Products</TableCell>
