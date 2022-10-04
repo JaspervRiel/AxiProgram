41c1f103f3b453bd359667058961c836699637fd
Yordi Verschure <459372@student.fontys.nl>
2022-10-04 09:11:48 +0200

    Navbar + Icon buttons

--- Frontend/axi/src/Components/GridButton.js
+++ Frontend/axi/src/Components/GridButton.js
@@ -1,11 +1,25 @@
 import './GridButton.css';
+import Button from '@mui/material/Button';
+import BatteryStdIcon from '@mui/icons-material/BatteryStd';
 
 function GridButton() {
     return (
+        
         <div class="grid-container">
-            <a href="/About" class="grid-item">Producten</a>
-            <a href="/About" class="grid-item">Orders</a>
-            <a href="/About" class="grid-item">Shipment</a>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+            
+            <Button
+                href="/Products" 
+                class="grid-item"
+                type="submit"
+                color="secondary"
+                variant="contained"
+                startIcon={<BatteryStdIcon/>}
+                > 
+            Producten </Button>
+
+            <a href="/Orders" class="grid-item">Orders</a>
+            <a href="/Shipment" class="grid-item">Shipment</a>
             <a href="/About" class="grid-item">4</a>
             <a href="/About" class="grid-item">5</a>
             <a href="/About" class="grid-item">6</a>
@@ -15,6 +29,7 @@ function GridButton() {
             <a href="/About" class="grid-item">10</a>
             <a href="/About" class="grid-item">11</a>
             <a href="/About" class="grid-item">12</a>
+            
         </div>
   );
 }
--- Frontend/axi/src/Components/Navbar.js
+++ Frontend/axi/src/Components/Navbar.js
@@ -7,9 +7,10 @@ function ColorSchemesExample() {
   return (
     <>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
-      <Navbar bg="white" variant="white" className="square border-bottom border-5">
+      <Navbar variant="white" className="square border-bottom border-5">
         <Container>
           <Navbar.Brand href="/">
+            
             <img
                 alt=""
                 src="https://www.partnersfontysict.nl/site/assets/files/1074/axi-logo.-logo_medium.jpg"
@@ -19,9 +20,10 @@ function ColorSchemesExample() {
               />{' '}
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="Producten" > Producten</Nav.Link>            
+            <Nav.Link href="Orders">Orders</Nav.Link>
+            <Nav.Link href="Shipment">Shipment</Nav.Link>
             <Nav.Link href="About">About</Nav.Link>
-            <Nav.Link href="pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
