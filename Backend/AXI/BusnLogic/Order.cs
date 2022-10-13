0f8ed6849f7893c198de47ea8046a5e69b9b0a98
Bas Bruin <487790@student.fontys.nl>
2022-10-13 14:57:15 +0200

    Order toegevoegd

---
+++ Backend/AXI/BusnLogic/Order.cs
@@ -0,0 +1,54 @@
+﻿using InterfaceLib;
+using InterfaceLib.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusnLogic
+{
+    public class Order
+    {
+        private int Id { get; set; }
+        private DateTime OrderDate { get; set; }
+        private DateTime? CompletedDate { get; set; }
+        private List<Product> Products { get; set; } = new List<Product>();
+
+        public Order(int id, DateTime orderDate, DateTime completedDate)
+        {
+            Id = id;
+            OrderDate = orderDate;
+            CompletedDate = completedDate;
+        }
+
+        public Order(OrderDTO dto)
+        {
+            this.Id = dto.Id;
+            this.OrderDate = dto.OrderDate;
+            this.CompletedDate = dto.CompletedDate;
+        }
+
+        public Order()
+        {
+
+        }
+
+        // Wordt gefixt wannneer we het nodig hebben
+        /*public OrderDTO GetDTO()
+        {
+            List<OrderDTO> dtos = new();
+            foreach(Product tempproduct in Products)
+            {
+                dtos.Add(new ProductDTO(tempproduct));
+            }
+            OrderDTO dto = new OrderDTO(Id, OrderDate, CompletedDate, Products);
+            return dto;
+        }*/
+
+        public override string? ToString()
+        {
+            return $"Id: {Id} OrderDate: {OrderDate}";
+        }
+    }
+}
---
+++ Backend/AXI/InterfaceLib/Container/IOrderContainer.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceLib.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceLib.Container
+{
+    public interface IOrderContainer
+    {
+        public List<OrderDTO> GetOrders();
+        public void Create(OrderDTO order);
+        public void Delete(int id);
+        public void Update(OrderDTO order);
+        public OrderDTO GetOrderById(int id);
+    }
+}
