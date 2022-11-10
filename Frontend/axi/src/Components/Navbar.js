7472a2acacec917b7d796c2e0909bd1dfb4884bc
Yordi Verschure <459372@student.fontys.nl>
2022-11-10 09:08:27 +0100

    Href logo axi

--- Frontend/axi/src/Components/Navbar.js
+++ Frontend/axi/src/Components/Navbar.js
@@ -9,7 +9,7 @@ function ColorSchemesExample() {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
       <Navbar variant="white" className="square border-bottom border-5">
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand href="https://www.axi.nl/nl">
             <img
                 alt=""
                 src="https://www.axi.nl/themes/custom/novsubtheme/img/logos/logo-axi-white.svg"
