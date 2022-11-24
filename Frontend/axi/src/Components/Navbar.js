b1016cefc352c200c8b326bddd922a947033ad4d
Fontys_yannick <y.leentvaar@student.fontys.nl>
2022-11-24 09:06:39 +0100

    small fix in order

--- Frontend/axi/src/Components/Navbar.js
+++ Frontend/axi/src/Components/Navbar.js
@@ -1,32 +1,36 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import './Navbar.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./Navbar.css";
 
 function ColorSchemesExample() {
   return (
     <>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        crossorigin="anonymous"
+      ></link>
       <Navbar variant="white" className="square border-bottom border-5">
         <Container>
           <Navbar.Brand href="https://www.axi.nl/nl">
             <img
-                alt=""
-                src="https://www.axi.nl/themes/custom/novsubtheme/img/logos/logo-axi-white.svg"
-                width="77"
-                height="34"
-                className="d-inline-block align-top"
-              />{' '}
+              alt=""
+              src="https://www.axi.nl/themes/custom/novsubtheme/img/logos/logo-axi-white.svg"
+              width="77"
+              height="34"
+              className="d-inline-block align-top"
+            />{" "}
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="Products">Producten</Nav.Link>            
-            <Nav.Link href="Orders">Orders</Nav.Link>
+            <Nav.Link href="Products">Producten</Nav.Link>
+            <Nav.Link href="Order">Orders</Nav.Link>
             <Nav.Link href="Levering">Levering</Nav.Link>
             <Nav.Link href="Locaties">Locaties</Nav.Link>
           </Nav>
           <Nav className="me-right">
             <Nav.Link href="Gebruiker">Gebruiker</Nav.Link>
-
           </Nav>
         </Container>
       </Navbar>
--- Frontend/axi/src/Order.js
+++ Frontend/axi/src/Order.js
@@ -2,6 +2,7 @@ import "./Order.css";
 import React from "react";
 import { useState } from "react";
 import { Button } from "@mui/material";
+import Navbar from "./Components/Navbar";
 
 function Order() {
   const [orders, setOrders] = useState([]);
@@ -48,66 +49,70 @@ function Order() {
   };
 
   return (
-    <div class="container">
-      <div class="left">
-        <div class="content">
-          <Button variant="outlined" onClick={Active}>
-            Actieve Orders
-          </Button>
-          <Button variant="outlined" onClick={completed}>
-            Oude Orders
-          </Button>
-        </div>
+    <div>
+      {" "}
+      <Navbar />
+      <div class="container">
+        <div class="left">
+          <div class="content">
+            <Button variant="outlined" onClick={Active}>
+              Actieve Orders
+            </Button>
+            <Button variant="outlined" onClick={completed}>
+              Oude Orders
+            </Button>
+          </div>
 
-        <table bgcolor="black" class="content">
-          <tr bgcolor="grey">
-            <th>Id</th>
-            <th>OrderDate</th>
-            <th>CompletedDate</th>
-          </tr>
-          {orders.map((item) => {
-            return (
-              <tr bgcolor="lightgrey" align="center">
-                <td>
-                  <Button onClick={() => ProductsFromOrder(item.Id)}>
-                    {JSON.stringify(item.Id)}
-                  </Button>
-                </td>
-                <td>{JSON.stringify(item.OrderDate)}</td>
-                <td>{JSON.stringify(item.CompletedDate)}</td>
-              </tr>
-            );
-          })}
-        </table>
-      </div>
-      <div class="right">
-        <table bgcolor="black" class="content">
-          <tr bgcolor="grey">
-            <th>ArtikelNaam</th>
-            <th>Locatie</th>
-            <th>Aantal</th>
-          </tr>
-          {Products.map((item) => {
-            return (
-              <tr bgcolor="lightgrey" align="center">
-                <td>{JSON.stringify(item.Name)}</td>
-                <td>{JSON.stringify(item.Location)}</td>
-                <td>{JSON.stringify(item.Stock)}</td>
-              </tr>
-            );
-          })}
-        </table>
-        <div class="content">
-          <Button
-            variant="outlined"
-            color="success"
-            onClick={() => OrderUpdate()}
-          >
-            Compleet
-          </Button>
-          <Button variant="outlined" color="error">
-            Niet Compleet
-          </Button>
+          <table bgcolor="black" class="content">
+            <tr bgcolor="grey">
+              <th>Id</th>
+              <th>OrderDate</th>
+              <th>CompletedDate</th>
+            </tr>
+            {orders.map((item) => {
+              return (
+                <tr bgcolor="lightgrey" align="center">
+                  <td>
+                    <Button onClick={() => ProductsFromOrder(item.Id)}>
+                      {JSON.stringify(item.Id)}
+                    </Button>
+                  </td>
+                  <td>{JSON.stringify(item.OrderDate)}</td>
+                  <td>{JSON.stringify(item.CompletedDate)}</td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+        <div class="right">
+          <table bgcolor="black" class="content">
+            <tr bgcolor="grey">
+              <th>ArtikelNaam</th>
+              <th>Locatie</th>
+              <th>Aantal</th>
+            </tr>
+            {Products.map((item) => {
+              return (
+                <tr bgcolor="lightgrey" align="center">
+                  <td>{JSON.stringify(item.Name)}</td>
+                  <td>{JSON.stringify(item.Location)}</td>
+                  <td>{JSON.stringify(item.Stock)}</td>
+                </tr>
+              );
+            })}
+          </table>
+          <div class="content">
+            <Button
+              variant="outlined"
+              color="success"
+              onClick={() => OrderUpdate()}
+            >
+              Compleet
+            </Button>
+            <Button variant="outlined" color="error">
+              Niet Compleet
+            </Button>
+          </div>
         </div>
       </div>
     </div>
